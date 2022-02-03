64e93dca8cd69ee3c6c505a197f27909e9e7fd56
PeterNdomano <peterndomano@gmail.com>
2022-02-03 17:40:45 +0300

    initial setup

---
+++ lib/index.ts
@@ -0,0 +1,10 @@
+export class Xapi {
+  password: string;
+  accountId: string;
+
+  constructor(password: string, accountId: string) {
+    this.password = password;
+    this.accountId = accountId;
+    console.log("Initialized");
+  }
+}
